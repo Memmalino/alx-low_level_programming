9be55104327897af56d78d57aecb1d6835b4ae49
Memmalino <125540335+Memmalino@users.noreply.github.com>
2023-03-17 00:18:05 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,13 +3,12 @@
 
 /**
  * main - it is used for fuct
- * return: it return 1
+ * return: the return is 1
  */
 
 int main(void)
 {
-	fprintf(stderr, "and that piece of art\\
-        is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
 
