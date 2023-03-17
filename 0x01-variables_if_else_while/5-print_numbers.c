89c6f701fa8045db592dd84ea95e33dfb2b34ced
memmalino <memmalino@gmail.com>
2023-03-17 14:16:33 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main func
+ * Return: it return 0
+ */
+int main(void)
+{
+	int numb;
+
+	while (numb < 10)
+	{
+		printf("%d", numb);
+		numb += 1;
+	}
+	printf("\n");
+	return (0);
+}
+
