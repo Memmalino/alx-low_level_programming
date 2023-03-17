dd6f7e0a6550656f92c73634f09b8bf462e13547
Memmalino <125540335+Memmalino@users.noreply.github.com>
2023-03-17 04:11:13 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 
 /**
  * main - it is used for fuct
- * return: the return is 1
+ * return: the successful return is 1
  */
 
 int main(void)
 {
- fputs(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
- return (1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
 
