0e8026ab9b7cf96c064cb23930a550678ec84390
Memmalino <125540335+Memmalino@users.noreply.github.com>
2023-03-17 20:30:58 +0100

    Create 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+include <stdio.h>
+
+/**
+ * main - this ia a main fnc
+ * Return: this returns 0
+ */
+
+int main(void)
+
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+		putchar(n);
+	for (n = 'A'; n <= 'Z'; n++)
+		putchar(n);
+	putchar('\n');
+
+	return (0);
+}
