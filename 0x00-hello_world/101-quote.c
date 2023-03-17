5407c8b79ec12c6d06bbcc839067fad57a3632a9
Memmalino <125540335+Memmalino@users.noreply.github.com>
2023-03-17 03:53:25 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-fputs(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-return (1);
+ fputs(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+ return (1);
 }
 
