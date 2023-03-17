c1f2ad934c3bd01b9f43c0787bd4c81ff6f0eca2
Memmalino <125540335+Memmalino@users.noreply.github.com>
2023-03-17 20:03:12 +0100

    Update 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main func
+ * Return: the return value is zero
+ */
+
+int main(void)
+{
+	char n;
+
+	for (n = 'a' ; n <= 'z' ; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
