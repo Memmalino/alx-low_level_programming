69ecf6595731d2125a9650678b0fd71feebc80d0
memmalino <memmalino@gmail.com>
2023-03-21 15:45:21 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 10-main.c 10-add.c -o 10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - It adds two values
+ * @n1: The first number
+ * @n2: The second number
+ * Return: it returns integer value
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
+
