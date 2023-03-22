8f2d9f35e9b6d2fa5cc9cb32bc7089c8da1c51e7
Memmalino <125540335+Memmalino@users.noreply.github.com>
2023-03-22 02:14:30 +0100

    Create 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+int n = 50;
+int fib[n];
+
+fib[0] = 1;
+fib[1] = 2;
+printf("%d, %d, ", fib[0], fib[1]);
+for (int i = 2; i < n; i++)
+{
+fib[i] = fib[i-1] + fib[i-2];
+printf("%d", fib[i]);
+If (i < n-1)
+{
+printf(", ");
+} 
+else
+{
+printf("\n");
+}
+}
+return 0;
+}
