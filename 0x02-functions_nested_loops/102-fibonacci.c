af9eac54924bddd4c1c4dcc823207ef1180ea659
Memmalino <125540335+Memmalino@users.noreply.github.com>
2023-03-22 03:14:32 +0100

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
 /**
- * main - main function 
+ * main - main function
  * Return: zero always
  */
 
 int main(void)
 {
- int n = 50;
- int fib[n];
+int n = 50;
+int fib[n];
 
- fib[0] = 1;
- fib[1] = 2;
+fib[0] = 1;
+fib[1] = 2;
 printf("%d, %d, ", fib[0], fib[1]);
 for (int i = 2; i < n; i++)
 {
-fib[i] = fib[i-1] + fib[i-2];
+fib[i] = fib[i - 1] + fib[i - 2];
 printf("%d", fib[i]);
 If (i < n - 1)
 {
@@ -23,7 +23,7 @@ printf(", ");
 } 
 else
 {
-printf("\n");
+printf('\n');
 }
 }
 return 0;
