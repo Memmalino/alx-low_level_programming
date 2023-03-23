80793ef7676d603c9640e8fa28f40c956c731d17
memmalino <memmalino@gmail.com>
2023-03-23 14:32:32 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - function that checked x
+ * @x: the argument to be checked
+ * Return: 1 or 0
+ */
+
+int _isupper(int x)
+{
+	if (x >= 65 && x <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
