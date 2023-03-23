c55c36db364ff5ab6a71097b297d2ddaef8d273f
Memmalino <125540335+Memmalino@users.noreply.github.com>
2023-03-23 01:12:19 +0100

    Create 103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - this is main funct
+ * Return: always returns 0
+ */
+
+int main(void)
+{
+	int m = 0;
+	long n = 1, o = 2, sum = k;
+
+	while (o + n < 4000000)
+	{
+	O += n;
+	if (o % 2 == 0)
+	sum += o;
+	n = o - n;
+	++i;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
