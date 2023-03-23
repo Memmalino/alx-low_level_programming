7263e1cf8cb5e27736cf9fc3f77dbffea801a257
memmalino <memmalino@gmail.com>
2023-03-23 14:59:14 -0700

    is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - it checks if an argument is a digit
+ * @x: the argument
+ * Return: 1 or 0
+ */
+
+int _isdigit(int x)
+{
+	if (x >= 48 && x <= 57)
+	{
+	return (1);
+	}
+	return (0);
+}
+
