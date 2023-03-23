d23ee8d27eed0f1ea69a41d2f963b5f068cb32b7
Memmalino <125540335+Memmalino@users.noreply.github.com>
2023-03-23 22:23:33 +0100

    Create _putchar.c

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * _putchar - a function that print c
+ * @c: The character to be printed
+ *
+ * Return: it returns 1 on success else -1
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
