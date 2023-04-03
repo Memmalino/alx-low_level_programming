cd58c2256c67266ba24dcb6f110bbc40d976dc77
memmalino <memmalino@gmail.com>
2023-04-03 14:42:46 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - assign a pointer value to a char
+ * @s: pointer to pointer
+ * @to: pointer char
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
+
