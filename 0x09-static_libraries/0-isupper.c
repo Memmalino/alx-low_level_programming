4f1f798819f772d9f75f2db38476377fcb319fcd
memmalino <memmalino@gmail.com>
2023-04-10 15:44:10 -0700

    A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - function that checked x
+ * @x: the argument to be checked
+ * Return: 1 or 0
+ */
+
+int _isupper(int x)
+{
+	if (x >= 65 && x <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
+
