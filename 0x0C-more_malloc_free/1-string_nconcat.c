79fca31518fb49cb3f4312b2b7d6bb0d88b674c1
memmalino <memmalino@gmail.com>
2023-04-13 15:43:09 -0700

    . string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -8,32 +8,29 @@
  * @n: The number of bytes from s2 to concatenate to s1
  * Return: pointer to the result string
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *s;
-	unsigned int i = 0, j = 0, len = 0, len2 = 0;
-
-	while (s1 && s1[len])
-		len++;
-	while (s2 && s2[len2])
-		len2++;
-	if (n < len2)
-		s = malloc(sizeof(char) * (len + n + 1));
-	else
-		s = malloc(sizeof(char) * (len + len2 + 1));
-	if (!s)
-		return (NULL);
-	while (i < len)
-	{
-		s[i] = s1[i];
-		i++;
-	}
-	while (n < len2 && i < (len + n))
-		s[i++] = s2[j++];
-	while (n >= len2 && i < (len + len2))
-		s[i++] = s2[i++];
-	s[i] = '\0';
-	return (s);
+char *sout;
+unsigned int ls1, ls2, lsout, i;
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+for (ls1 = 0; s1[ls1] != '\0'; ls1++)
+;
+for (ls2 = 0; s2[ls2] != '\0'; ls2++)
+;
+if (n > ls2)
+n = ls2;
+lsout = ls1 + n;
+sout = malloc(lsout + 1);
+if (sout == NULL)
+return (NULL);
+for (i = 0; i < lsout; i++)
+if (i < ls1)
+sout[i] = s1[i];
+else
+sout[i] = s2[i - ls1];
+sout[i] = '\0';
+return (sout);
 }
-
