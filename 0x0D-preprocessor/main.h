625183e5ac533f9ae75546d889cb3cac534f8e56
memmalino <memmalino@gmail.com>
2023-04-17 13:43:21 -0700

    Function-like macro, Sum, Pi, File name, Object-like Macro

--- 0x0D-preprocessor/main.h
+++ 0x0D-preprocessor/main.h
@@ -1,4 +1,9 @@
 #pragma once
 
-int _putchar(char c);
+#include <stdio.h>
+#include "0-object_like_macro.h"
+#include "1-pi.h"
+#include "2-main.c"
+#include "3-function_like_macro.h"
+#include "4-sum.h"
 
