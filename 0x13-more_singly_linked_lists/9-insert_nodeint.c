8dc6b14d1b1627fdedf15cbb5ed67ccb6fc94e66
memmalino <memmalino@gmail.com>
2023-05-02 14:24:22 -0700

    insertion of new node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -4,57 +4,45 @@
  * insert_nodeint_at_index - Funct that inserts a new mode at nth position
  * @head: pointer to the first data in the linked list
  * @idx: index of the list to add to
+ * @n: the last parameter integer
  * Return: address of the new node if successful else NULL
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
-
 	listint_t *ptr, *tmp;
 
 	if (head == NULL)
-	{
 		return (NULL);
-	}
-
 	if (idx == 0)
 	{
 	ptr = malloc(sizeof(listint_t));
 
 	if (ptr == NULL)
-	{
 		return (NULL);
-	}
 	ptr->n = n;
 	ptr->next = *head;
 	*head = ptr;
 	return (ptr);
 	}
 
-	temp = *head;
-	for (i = 0; i < idx - 1; o++)
+	tmp = *head;
+	for (i = 0; i < idx - 1; i++)
 	{
-		if (temp == NULL)
-		{
+		if (tmp == NULL)
 			return (NULL);
-		}
-	temp = temp->next;
+	tmp = tmp->next;
 	}
 
-	if (temp == NULL)
-	{
+	if (tmp == NULL)
 		return (NULL);
-	}
 	ptr = malloc(sizeof(listint_t));
 	if (ptr == NULL)
-	{
 		return (NULL);
-	}
 	ptr->n = n;
-	ptr->next = temp->next;
-	temp->next = ptr;
+	ptr->next = tmp->next;
+	tmp->next = ptr;
 
 	return (ptr);
 }
-
