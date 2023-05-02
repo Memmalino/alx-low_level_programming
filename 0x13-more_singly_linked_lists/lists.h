955a2c81445b35670123ae348598869e1efef4d5
memmalino <memmalino@gmail.com>
2023-05-02 04:12:19 -0700

    correction

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef LISTS_H
+#define LISTS_H
 
 #include <stddef.h>
 #include <stdio.h>
@@ -36,3 +37,6 @@ size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
+
+#endif
+
