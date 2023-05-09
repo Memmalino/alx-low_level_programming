a7e119d196dacb425d861edd23b7e00843274506
memmalino <memmalino@gmail.com>
2023-05-08 19:29:20 -0700

    updated

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,5 +10,6 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
+void print_elf_header_info(Elf64_Ehdr *elf_header);
+int read_elf_header(int fd, Elf64_Ehdr *elf_header);
 #endif
