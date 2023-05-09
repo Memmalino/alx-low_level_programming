601f53a748d85a710da28e4f6d98ab8e43038b3e
memmalino <memmalino@gmail.com>
2023-05-08 17:14:26 -0700

    cp function

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,6 +2,10 @@
 #define MAIN_H
 #include <stddef.h>
 #include <unistd.h>
+#define BUFFER_SIZE 1024
+
+void close_fd(int fd);
+void copy_file(const char *from_path, const char *to_path);
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
