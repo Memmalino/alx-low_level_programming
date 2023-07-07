c9dc1e6bbca54e6bc41f8128a82736cb8c1df0a5
memmalino <memmalino@gmail.com>
2023-07-07 21:15:00 +0100

    bit project

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * get_bit - funct that returns the value of a bit
+ * @n: the number to work on
+ * @index: the index starting from 0
+ * Return: value of bit at index or -1
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int mask;
+
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+	return (-1);
+	}
+
+	mask = 1UL << index;
+
+	return ((n & mask) != 0);
+}
---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * _putchar - a func that prints to the stdout
+ * @c: The character to be printed
+ * Return: return 1 on succes -1 on error
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
