1f51a315bd040f64d6697033ce641aefb02ca910
I'm not a Human <125540335+Memmalino@users.noreply.github.com>
2023-08-21 23:50:27 +0100

    Update _strlen.c

--- 0x18-dynamic_libraries/_strlen.c
+++ 0x18-dynamic_libraries/_strlen.c
@@ -1,5 +1,19 @@
 #include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+
 int _strlen(char *s)
 {
-	return (0);
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
 }
