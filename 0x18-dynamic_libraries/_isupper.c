bac2cfd03a490edca7c51555d2af2b0570dcf5d3
I'm not a Human <125540335+Memmalino@users.noreply.github.com>
2023-08-21 23:32:05 +0100

    Update _isupper.c

--- 0x18-dynamic_libraries/_isupper.c
+++ 0x18-dynamic_libraries/_isupper.c
@@ -1,6 +1,16 @@
 #include "main.h"
 
+/**
+ * _isupper - function that checked x
+ * @c: the argument to be checked
+ * Return: 1 or 0
+ */
+
 int _isupper(int c)
 {
+	if (c >= 65 && c <= 90)
+	{
+	return (1);
+	}
 	return (0);
 }
