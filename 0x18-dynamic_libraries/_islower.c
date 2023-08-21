7c7808ffb865b5d77a97aca515d0b56a6d617189
I'm not a Human <125540335+Memmalino@users.noreply.github.com>
2023-08-21 23:31:02 +0100

    Update _islower.c

--- 0x18-dynamic_libraries/_islower.c
+++ 0x18-dynamic_libraries/_islower.c
@@ -1,5 +1,16 @@
 #include "main.h"
+
+/**
+ * _islower - it checks for lower case character
+ * @c: The character to be checked
+ * Return: 1 for lowercase or 0 for something else
+ */
+
 int _islower(int c)
 {
+	if (c >= 97 && c <= 122)
+	{
+	return (1);
+	}
 	return (0);
 }
