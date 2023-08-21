9ed186368022a80c6cde607672db2f7215ac43a6
I'm not a Human <125540335+Memmalino@users.noreply.github.com>
2023-08-21 23:46:02 +0100

    Update _puts.c

--- 0x18-dynamic_libraries/_puts.c
+++ 0x18-dynamic_libraries/_puts.c
@@ -1,5 +1,13 @@
 #include "main.h"
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: string to print
+ */
 void _puts(char *s)
 {
-	return (0);
+	while (*s != '\0')
+	{
+		_putchar(*s++);
+	}
+		_putchar('\n');
 }
