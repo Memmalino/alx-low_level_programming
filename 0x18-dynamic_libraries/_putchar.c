2f426965ff6b161ed21998ec1a10d4768fcc31ce
I'm not a Human <125540335+Memmalino@users.noreply.github.com>
2023-08-21 23:36:54 +0100

    Update _putchar.c

--- 0x18-dynamic_libraries/_putchar.c
+++ 0x18-dynamic_libraries/_putchar.c
@@ -1,5 +1,13 @@
 #include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - work exactly like putchar function
+ * @c: The character to print
+ * Return: 1 on succes
+ * On error, -1 is returned
+ */
+
 int _putchar(char c)
 {
-	return (0);
+	return (write(1, &c, 1));
 }
