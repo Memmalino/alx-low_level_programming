40dfc05cd5a3cac7ddd30bee435762e24b6a6cc3
I'm not a Human <125540335+Memmalino@users.noreply.github.com>
2023-08-21 23:30:02 +0100

    Update _isdigit.c

--- 0x18-dynamic_libraries/_isdigit.c
+++ 0x18-dynamic_libraries/_isdigit.c
@@ -1,6 +1,16 @@
 #include "main.h"
 
+/**
+ * _isdigit - it checks if an argument is a digit
+ * @c: the argument
+ * Return: 1 or 0
+ */
+
 int _isdigit(int c)
 {
+	if (c >= 48 && c <= 57)
+	{
+	return (1);
+	}
 	return (0);
 }
